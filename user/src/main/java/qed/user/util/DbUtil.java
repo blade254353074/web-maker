69afc48be475087bff4b97581fd89a973c250bbe
qed <taoyingqi2013@sina.com>
2015-09-02 14:48:17 +0800

    new Maven Project! to Extradimensional

---
+++ user/src/main/java/qed/user/util/DbUtil.java
@@ -0,0 +1,42 @@
+package qed.user.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DbUtil {
+	public static Connection getConnection() throws SQLException {
+		Connection conn = null;
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/qed_test", "root", "123456");
+		return conn;
+	}
+
+	public static void close(Connection con) {
+		try {
+			if (con != null)
+				con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void close(PreparedStatement ps) {
+		try {
+			if (ps != null)
+				ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void close(ResultSet rs) {
+		try {
+			if (rs != null)
+				rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
